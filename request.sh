c581b3ce1900154dacd92981948e5489f38bd258
Danilo Horta <danilo.horta@pm.me>
2020-05-25 12:13:20 +0100

    extract url

--- request.sh
+++ request.sh
@@ -1,3 +1,9 @@
 #/usr/bin bash
 
-curl 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:76.0) Gecko/20100101 Firefox/76.0' -H 'Accept: application/json, text/plain, */*' -H 'Accept-Language: en-GB,en;q=0.5' --compressed -H 'X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm' -H 'Origin: https://covid.saude.gov.br' -H 'Connection: keep-alive' -H 'Referer: https://covid.saude.gov.br/' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'TE: Trailers'
+curl 'https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalGeral' \
+    -H 'Accept: application/json, text/plain, */*' \
+    -H 'Accept-Language: en-GB,en;q=0.5' \
+    --compressed -H 'X-Parse-Application-Id: unAFkcaNDeXajurGB7LChj8SgQYS2ptm' \
+    -H 'Origin: https://covid.saude.gov.br' -H 'Connection: keep-alive' \
+    -H 'Referer: https://covid.saude.gov.br/' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' \
+    -H 'TE: Trailers' | python3 -c "import sys, json; print(json.load(sys.stdin)['results'][0]['arquivo']['url'])"
